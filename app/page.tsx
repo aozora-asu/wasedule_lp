b8b04bc08c5de71eeb54fbd50ca13b55a6802f45
SotaMatsui <helvetica.3716@gmail.com>
2025-03-19 19:48:56 +0900

    update: 基本的なレイアウトを実装
    メニューとかフッターとか

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,26 @@
 import { Button } from '@/components/ui/button'
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle
-} from '@/components/ui/navigation-menu'
-
 import { ArrowRight } from 'lucide-react'
 import Image from 'next/image'
-import Link from 'next/link'
 
 export default function Home () {
   return (
     <>
-      <header className='flex justify-center w-full sticky'>
-        <div className='flex w-full max-w-7xl gap-3 justify-between py-4'>
-          <Image
-            src='/wasedule_icon.svg'
-            width={42}
-            height={42}
-            alt='わせジュール'
-          />
-          <NavigationMenu>
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>使い方ガイド</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <NavigationMenuLink>Link</NavigationMenuLink>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <Link href='/' legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    メディア
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <Link href='/' legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    お問い合わせ
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
-        </div>
-      </header>
       <main className='flex justify-center'>
-        <section className='flex w-full max-w-7xl justify-center items-center gap-12'>
-          <div className='my-8'>
-            <span className='text-5xl'>
-              早稲田生のための
-              <br />
-              授業課題管理アプリ。
-            </span>
-            <div className='flex gap-4 my-6'>
+        <section className='flex w-full max-w-7xl justify-center items-center flex-col lg:flex-row lg:gap-12'>
+          <div className='mt-8 mx-2 px-2'>
+            <div className='flex flex-col gap-6 text-center lg:text-start'>
+              <span className='text-5xl  lg:text-6xl font-semibold palt'>
+                時間割も
+                <br />
+                授業課題も
+                <br />
+                バイトも
+                <br />
+                これひとつで。
+              </span>
+              <span className='break-keep'>早稲田生のための授業課題管理アプリ、<b>わせジュール</b>。</span>
+            </div>
+            <div className='flex flex-wrap gap-4 my-6  justify-center lg:justify-start'>
               <Button>
                 Google Playでダウンロード
                 <ArrowRight />
@@ -71,21 +33,12 @@ export default function Home () {
           </div>
           <Image
             src='/screenshots/simulator_screenshot_CB3AC48E-3027-41C3-91DA-2698E7A67303-portrait.png'
-            width={480}
-            height={480}
+            width={420}
+            height={420}
             alt='わせジュール'
           />
         </section>
       </main>
-      <footer className='flex flex-col justify-center items-center px-4 py-8'>
-        <section className='flex flex-wrap max-w-7xl'>
-          <Link href="" className='p-3 text-sm font-semibold'>プライバシーポリシー</Link>
-          <Link href="" className='p-3 text-sm font-semibold'>利用規約</Link>
-        </section>
-        <section className='w-full max-w-7xl text-end'>
-          <p className='text-sm font-semibold'>© 2024 わせジュール</p>
-        </section>
-      </footer>
     </>
   )
 }

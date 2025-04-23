f1e2f774e8dfa178c3d75c6f8e599baf9daf3b9f
SotaMatsui <helvetica.3716@gmail.com>
2025-04-23 11:40:47 +0900

    cms型定義修正

--- lib/microcms.ts
+++ lib/microcms.ts
@@ -1,39 +1,43 @@
 import { createClient } from 'microcms-js-sdk';
 
 export type Blog = {
-    id: string;
-    title: string;
-    body: string;   
+  id: string;
+  title: string;
+  body: string;
+  category: {
+    "category-name": string
+  };
+  summary: string;
 }
 
 if (!process.env.MICROCMS_DOMAIN) {
-    throw new Error("MICROCMS_DOMAIN is required");
+  throw new Error("MICROCMS_DOMAIN is required");
 }
 
 if (!process.env.MICROCMS_API_KEY) {
-    throw new Error("MICROCMS_API_KEY is required");
+  throw new Error("MICROCMS_API_KEY is required");
 }
 
 export const client = createClient({
-    serviceDomain: process.env.MICROCMS_DOMAIN,
-    apiKey: process.env.MICROCMS_API_KEY ,
+  serviceDomain: process.env.MICROCMS_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
 });
 
 // 記事一覧を取得
 export const getArticles = async () => {
-    const articles = await client.getList<Blog>({
+  const articles = await client.getList<Blog>({
     endpoint: "guides"
-    });
-    return articles;
+  });
+  return articles;
 }
 
 // 記事の詳細を取得
 export const getDetail = async (contentId: string) => {
-    const blog = await client.getListDetail<Blog>({
-        endpoint: "guides",
-        contentId,
-    });
-    return blog;
+  const blog = await client.getListDetail<Blog>({
+    endpoint: "guides",
+    contentId,
+  });
+  return blog;
 };
 
 

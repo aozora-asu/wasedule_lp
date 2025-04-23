8fbb7364dd02a4fef992a3aca18f5c19fa898f07
SotaMatsui <helvetica.3716@gmail.com>
2025-04-23 15:14:58 +0900

    ガイドのリッチテキスト用のCSSを追加定義

--- app/guides/[articleId]/page.tsx
+++ app/guides/[articleId]/page.tsx
@@ -35,6 +35,13 @@ export default async function StaticDetailPage({
         [&>blockquote]:my-6
         [&>blockquote]:bg-neutral-100
         [&>blockquote]:rounded-md
+        [&>h4]:text-lg
+        [&>h4]:font-semibold
+        [&>h4]:py-2
+        [&>hr]:my-6
+        [&_code]:px-2
+        [&_code]:rounded-sm
+        [&_code]:bg-gray-100
         "
       />
     </>

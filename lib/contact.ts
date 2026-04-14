205a7cd4357a81990105ef86dbfba8bdfbece7f4
sota <sota.matsui@akane.waseda.jp>
2026-04-14 12:10:23 +0900

    feat: お問い合わせ通知をDiscordに移行

--- lib/contact.ts
+++ lib/contact.ts
@@ -1,7 +1,9 @@
 import { Resend } from "resend";
 import { sendLineNotification } from "./line";
-import { postContact } from "./microcms";
+import { ContactPostInput, postContact } from "./microcms";
 import { EmailTemplate } from "@/components/email-template";
+import { contactSpreadsheet } from "./google-apps-script";
+import { discord } from "./discord";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const from = process.env.CONTACT_EMAIL_FROM;
@@ -18,7 +20,7 @@ export type ContactPayload = {
 export async function processContactInquiry(payload: ContactPayload) {
   const { subject, name, email, body, sentFrom, userEnv } = payload;
   try {
-    const postContactResult = await postContact({
+    const dataToSave: ContactPostInput = {
       title: subject,
       name,
       email,
@@ -26,24 +28,30 @@ export async function processContactInquiry(payload: ContactPayload) {
       sentAt: new Date(),
       sentFrom: sentFrom === "landing" ? ["landing"] : ["app"],
       userEnv,
-    });
+    };
 
-    const microCMSDomain = process.env.MICROCMS_DOMAIN || "unknown";
+    // お問い合わせ内容をmicroCMSに保存
+    const postContactResult = await postContact(dataToSave);
 
-    const lineMessage = getLineMessage({
-      ...payload,
-      microCMSDomain,
-      microCMSItemId: postContactResult.id,
-    });
+    // お問い合わせ内容をGoogleスプレッドシートに保存
+    await contactSpreadsheet.create(dataToSave);
 
-    await sendLineNotification(lineMessage);
+    // LINE通知送信
+    // const lineMessage = getLineMessage({
+    //   ...payload,
+    //   microCMSDomain: process.env.MICROCMS_DOMAIN || "unknown",
+    //   microCMSItemId: postContactResult.id,
+    // });
+    // await sendLineNotification(lineMessage);
 
+    // Discord通知送信
+    await discord.notifyContactSubmission(dataToSave);
+
+    // ユーザーへの自動返信メール送信
     const react = EmailTemplate({ name, subject, email, body });
-
-    if(!from) {
+    if (!from) {
       throw new Error("CONTACT_EMAIL_FROM is not defined");
     }
-
     const data = await resend.emails.send({
       from: `わせジュールサポート <${from}>`,
       to: email,
---
+++ lib/google-apps-script.ts
@@ -0,0 +1,32 @@
+import { ContactPostInput } from "./microcms";
+
+const GAS_SCRIPT_URL = process.env.GAS_SCRIPT_URL;
+const GAS_SECRET_TOKEN = process.env.GAS_SECRET_TOKEN;
+
+if (!GAS_SCRIPT_URL) {
+  throw new Error("GAS_SCRIPT_URL is not defined");
+}
+
+if (!GAS_SECRET_TOKEN) {
+  throw new Error("GAS_SECRET_TOKEN is not defined");
+}
+
+
+const create = async (data: ContactPostInput) => {
+  const res = await fetch(GAS_SCRIPT_URL, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "text/plain;charset=utf-8",
+    },
+    body: JSON.stringify({token: GAS_SECRET_TOKEN, ...data}),
+  });
+
+  console.log("GAS response status:", await res.text());
+
+  if (!res.ok) {
+    throw new Error("Failed to submit contact form");
+  }
+};
+
+export const contactSpreadsheet = { create };
